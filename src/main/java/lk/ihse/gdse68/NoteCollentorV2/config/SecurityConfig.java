94c4347d745e0ecde89be2f443c5f14a2c6b0aa4
MalithShehan <shehansandakalum2003@gmail.com>
2024-10-13 10:46:36 +0530

    Add InMemoryUserDetailsManager with basic auth test with Value injection

---
+++ src/main/java/lk/ihse/gdse68/NoteCollentorV2/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package lk.ihse.gdse68.NoteCollentorV2.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+    @Value("${secure.basic.username}")
+    String userName;
+    @Value("${secure.basic.password}")
+    String password;
+    @Value("${secure.basic.role}")
+    String role;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager () {
+        UserDetails principleUSer = User.withDefaultPasswordEncoder()
+                .username(userName)
+                .password(password)
+                .roles(role)
+                .build();
+        return new InMemoryUserDetailsManager(principleUSer);
+    }
+
+}
