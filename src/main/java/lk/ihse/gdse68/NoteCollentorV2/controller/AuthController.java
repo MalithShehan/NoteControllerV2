bd8361f528c0cff92141789fdc46bbee09fd3f67
MalithShehan <shehansandakalum2003@gmail.com>
2024-10-13 12:49:17 +0530

    Add controllers for auth request and create model for SignIn

--- src/main/java/lk/ihse/gdse68/NoteCollentorV2/controller/AuthController.java
+++ src/main/java/lk/ihse/gdse68/NoteCollentorV2/controller/AuthController.java
@@ -1,21 +1,46 @@
 package lk.ihse.gdse68.NoteCollentorV2.controller;
 
+import lk.ihse.gdse68.NoteCollentorV2.dto.impl.UserDTO;
+import lk.ihse.gdse68.NoteCollentorV2.exception.DataPersistFailedException;
 import lk.ihse.gdse68.NoteCollentorV2.jwtModels.JWTResponse;
+import lk.ihse.gdse68.NoteCollentorV2.jwtModels.SignIn;
+import lk.ihse.gdse68.NoteCollentorV2.util.AppUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("api/v1/auth")
 public class AuthController {
-    @PostMapping(value = "signUp")
-    public ResponseEntity<JWTResponse> signUp() {
-        return null;
+    @PostMapping(value = "signUp", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<JWTResponse> signUp(
+            @RequestPart("firstName") String firstName,
+            @RequestPart ("lastName") String lastName,
+            @RequestPart ("email") String email,
+            @RequestPart ("password") String password,
+            @RequestPart ("profilePic") MultipartFile profilePic)
+    {
+        try {
+            byte [] imageByteCollection = profilePic.getBytes();
+            String base64ProfilePic = AppUtil.toBase64ProfilePic(imageByteCollection);
+            UserDTO buildUserDTO = new UserDTO();
+            buildUserDTO.setFirstName(firstName);
+            buildUserDTO.setLastName(lastName);
+            buildUserDTO.setEmail(email);
+            buildUserDTO.setPassword(password);
+            buildUserDTO.setProfilePic(base64ProfilePic);
+            userService.saveUser(buildUserDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistFailedException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
     @PostMapping(value = "signIn")
-    public ResponseEntity<JWTResponse> signIn(){
+    public ResponseEntity<JWTResponse> signIn(@RequestBody SignIn signIn){
         return null;
     }
-
 }
---
+++ src/main/java/lk/ihse/gdse68/NoteCollentorV2/jwtModels/SignIn.java
@@ -0,0 +1,15 @@
+package lk.ihse.gdse68.NoteCollentorV2.jwtModels;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class SignIn {
+    private String email;
+    private String password;
+}
