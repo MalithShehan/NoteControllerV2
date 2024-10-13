c96d6c0805c644eb0236d8bd20827040ce51644b
MalithShehan <shehansandakalum2003@gmail.com>
2024-10-13 15:33:33 +0530

    Add cnfigs for JWT generation and other process

--- src/main/java/lk/ihse/gdse68/NoteCollentorV2/config/JWTConfig.java
+++ src/main/java/lk/ihse/gdse68/NoteCollentorV2/config/JWTConfig.java
@@ -1,4 +1,24 @@
 package lk.ihse.gdse68.NoteCollentorV2.config;
 
-public class JWTConfig {
+import ch.qos.logback.core.util.StringUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Configuration
+@RequiredArgsConstructor
+public class JWTConfig extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String initToken = request.getHeader("Authorization");
+        String userEmail;
+        String jwtToken;
+
+        if(StringUtil.isEmty(initToken)) || !initToken.startsWith("Barer")
+    }
 }
